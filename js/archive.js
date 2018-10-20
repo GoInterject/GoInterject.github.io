398031a77261e6c57de3079aeed08fe4a8780ba6
Eugene Lyubar <lyubar@gmail.com>
2018-10-20 16:01:54 -0700

    Remove old references

--- js/archive.js
+++ js/archive.js
@@ -7,7 +7,7 @@ if (window.navigator.onLine) {
   var suppressButterBar = false;
   /* This JSON file contains a current list of all docs versions of Docker */
   $.getJSON("/js/archives.json", function(result){
-    var outerDivStart = '<div id="archive-butterbar"><div class="container"><div style="text-align: center"><span id="archive-list">This is <b><a href="https://docs.docker.com/docsarchive/" style="color: #254356; text-decoration: underline !important">archived documentation</a></b> for Docker&nbsp;' + dockerVersion + '. Go to the <a style="color: #254356; text-decoration: underline !important" href="https://docs.docker.com/">latest docs</a> or a different version:&nbsp;&nbsp;</span>' +
+    var outerDivStart = '<div id="archive-butterbar"><div class="container"><div style="text-align: center"><span id="archive-list">This is <b><a href="https://docs.gointerject.com/docsarchive/" style="color: #254356; text-decoration: underline !important">archived documentation</a></b> for Docker&nbsp;' + dockerVersion + '. Go to the <a style="color: #254356; text-decoration: underline !important" href="https://docs.gointerject.com/">latest docs</a> or a different version:&nbsp;&nbsp;</span>' +
                                '<span style="z-index: 1001" class="dropdown">';
     var listStart = '<ul class="dropdown-menu" role="menu" aria-labelledby="archive-menu">';
     var listEnd = '</ul>';
@@ -25,9 +25,9 @@ if (window.navigator.onLine) {
           // Get the button code
           buttonCode = '<button id="archive-menu" data-toggle="dropdown" class="btn dropdown-toggle" style="border: 1px solid #254356; background-color: #fff; color: #254356;">' + prettyName + '&nbsp;(current) &nbsp;<span class="caret"></span></button>';
           // The link is different for the current release
-          listItems.push('<li role="presentation"><a role="menuitem" tabindex="-1" href="https://docs.docker.com/">' + prettyName + '</a></li>');
+          listItems.push('<li role="presentation"><a role="menuitem" tabindex="-1" href="https://docs.gointerject.com/">' + prettyName + '</a></li>');
         } else {
-          listItems.push('<li role="presentation"><a role="menuitem" tabindex="-1" href="https://docs.docker.com/' + field.name + '/">' + prettyName + '</a></li>');
+          listItems.push('<li role="presentation"><a role="menuitem" tabindex="-1" href="https://docs.gointerject.com/' + field.name + '/">' + prettyName + '</a></li>');
         }
       }
     });
