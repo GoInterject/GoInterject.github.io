8db8b1a705e7644de75b60aeeae356f75d378a59
derekolson09 <d.g.olson09@gmail.com>
2019-01-07 17:25:46 -0800

    update(logo): Removed modal from logo image

--- js/stylemod.js
+++ js/stylemod.js
@@ -5,22 +5,25 @@ $(document).ready(function(){
     $('img').click(function() {
         // get current img
         var thisImg = $(this);
-
-        // Get the modal
-        var modal = document.getElementById('modal-main');
-        var modalImg = document.getElementById("modal-img");
+            // ensure that this is not the logo image
+            if (!$(thisImg).hasClass('logo')) {
         
-        // Get the image and insert it inside the modal 
-        modal.style.display = "block";
-        modalImg.src = thisImg.attr('src');
+            // Get the modal
+            var modal = document.getElementById('modal-main');
+            var modalImg = document.getElementById("modal-img");
+            
+            // Get the image and insert it inside the modal 
+            modal.style.display = "block";
+            modalImg.src = thisImg.attr('src');
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close-modal")[0];
-        
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() { 
-            modal.style.display = "none";
-        }
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close-modal")[0];
+            
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() { 
+                modal.style.display = "none";
+            }
+        };
     });
 });
 
