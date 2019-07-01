1137d95012351d1c279c539c409a74557beb9848
leilanihagen <leilanih@interject.local>
2019-07-01 09:56:10 -0700

    update(code): working on conditional nav render for search page.

--- js/toc.js
+++ js/toc.js
@@ -6,22 +6,39 @@ layout: null
 // define the nav container variable
 var docstoc
 
-// check to see if this is part of the default nave
+// check to see if this is part of the default nav
 if (sitePath.startsWith("/w") == true){
     docstoc = {{ site.data.toc | jsonify }};
-} 
+}
 // if not part of the default nav check if it's an app
 else if (sitePath.startsWith("/bApps/bFinancials") == true){
     docstoc = {{ site.data.apptoc.financial | jsonify }};
-} 
+}
 else if (sitePath.startsWith("/bApps/WCNTraining") == true){
     docstoc = {{ site.data.apptoc.WCNTraining | jsonify }};
 } 
+// if not default or an app, check if it is the search page
+else if (sitePath.startsWith("/schemas/search") == true){
+    // now determine the referring (origination?) page to determine which docstoc to assign
+    if (referringPage.startsWith("/w") == true){
+        docstoc = {{ site.data.toc | jsonify }};
+    }
+    else if (referringPage.startsWith("/bApps/bFinancials") == true){
+        docstoc = {{ site.data.apptoc.financial | jsonify }};
+    }
+    else if (referringPage.startsWith("/bApps/WCNTraining") == true){
+        docstoc = {{ site.data.apptoc.WCNTraining | jsonify }};
+    }
+}
 // if it is not an app nor a default page, then load the default nav
  else {
     docstoc = {{ site.data.toc | jsonify }};
 }
 
-renderNav(docstoc);
+// var temp = sitePath.startsWith
 
+// if (sitePath.startsWith("/schemas/search") == true){
+    
+
+renderNav(docstoc);
 
