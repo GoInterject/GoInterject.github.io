cfc9d814991dee62cf8c1fc98d433a87bcb6ed12
leilanihagen <leilanih@interject.local>
2019-07-01 15:48:59 -0700

    update(code): enable conditional rendering of vert. nav on search page.

--- js/toc.js
+++ js/toc.js
@@ -6,7 +6,7 @@ layout: null
 // define the nav container variable
 var docstoc
 
-// check to see if this is part of the default nave
+// check to see if this is part of the default nav
 if (sitePath.startsWith("/w") == true){
     docstoc = {{ site.data.toc | jsonify }};
 } 
@@ -16,11 +16,27 @@ else if (sitePath.startsWith("/bApps/bFinancials") == true){
 } 
 else if (sitePath.startsWith("/bApps/InterjectTraining") == true){
     docstoc = {{ site.data.apptoc.InterjectTraining | jsonify }};
-} 
+}
 // if it is not an app nor a default page, then load the default nav
  else {
     docstoc = {{ site.data.toc | jsonify }};
 }
+
+// search page case and subcases
+if (sitePath.startsWith("/schemas/search") == true){
+    // now determine the referring (origination?) page to determine which docstoc to assign
+    if (referringPage.includes("/w") == true){
+        docstoc = {{ site.data.toc | jsonify }};
+    }
+    else if (referringPage.includes("/bApps/bFinancials") == true){
+        docstoc = {{ site.data.apptoc.financial | jsonify }};
+    }
+    else if (referringPage.includes("/bApps/InterjectTraining") == true){
+        docstoc = {{ site.data.apptoc.InterjectTraining | jsonify }};
+    }
+    else {
+        docstoc = {{ site.data.toc | jsonify }};
+    }
+}
+
 renderNav(docstoc);
-
-
