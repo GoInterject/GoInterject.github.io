550ea3ae7ca3b6a9e6ae0e4e38c19cee6522a4d8
leilanihagen <leilanih@interject.local>
2019-07-01 17:32:58 -0700

    update(code): add conditional rendering of horiz. nav on search results page.

--- js/toc.js
+++ js/toc.js
@@ -8,18 +8,18 @@ var docstoc
 
 // check to see if this is part of the default nav
 if (sitePath.startsWith("/w") == true){
-    docstoc = {{ site.data.toc | jsonify }};
-} 
+   docstoc = {{ site.data.toc | jsonify }};
+}
 // if not part of the default nav check if it's an app
 else if (sitePath.startsWith("/bApps/bFinancials") == true){
-    docstoc = {{ site.data.apptoc.financial | jsonify }};
-} 
+   docstoc = {{ site.data.apptoc.financial | jsonify }};
+}
 else if (sitePath.startsWith("/bApps/InterjectTraining") == true){
     docstoc = {{ site.data.apptoc.InterjectTraining | jsonify }};
 }
 // if it is not an app nor a default page, then load the default nav
- else {
-    docstoc = {{ site.data.toc | jsonify }};
+else {
+   docstoc = {{ site.data.toc | jsonify }};
 }
 
 // search page case and subcases
